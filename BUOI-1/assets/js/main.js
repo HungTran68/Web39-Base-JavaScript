dd78b28cf38aeb6b8cc40312a91c904b87c38d16
HungTran68 <161321790+HungTran68@users.noreply.github.com>
2024-03-19 21:15:59 +0700

    javascript-buoi-1

---
+++ BUOI-1/assets/js/main.js
@@ -0,0 +1,70 @@
+let age = 35;
+console.log(age);
+// Bien toan cuc
+//  duoc khai bao ben ngoai function va khoi block
+
+// Bien cuc bo
+
+// khai bao function
+
+function Hello() {
+    let chaoxin = 'xin chao viet nam';
+    console.log(chaoxin);
+};
+// thuc thi
+Hello();
+
+// Khai bao function name;
+function name() {
+    let count = 39;
+    console.log(count);
+}
+
+name();
+
+// VD3
+// let city;
+
+function sayHello() {
+    city = "Hanoi";
+    console.log('goodnight');
+}
+
+sayHello();
+
+console.log(city);
+
+// Cac kieu du lieu co ban cua javascript
+
+/*
++ kieu du lieu String :
+*/ 
+
+// let a = 'hom nay troi mua';
+
+// console.log(a);
+
+// let b = 'thoi tiet dep';
+
+
+// VD4
+
+let message = "xin chao cac ban";
+let nameA = "tao la hung"
+
+console.log(message +" "+ nameA);
+
+
+/****************************/
+
+/* TEMPLATE STRING ES6*/
+
+let newName = 'web39';
+console.log('Hello 500 ae lop web39');
+
+console.log(`hello 500 ae ${newName} `)
+
+/* Kieu du lieu number*/ 
+
+let num = 20;
+console.log(num);
